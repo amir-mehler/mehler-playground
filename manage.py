1bcb88a81397decd5b5ff270a04da0201c6d33d8
Amir Mehler <6296994+amir-mehler@users.noreply.github.com>
2020-03-22 14:37:49 +0200

    added time for the build to run

--- manage.py
+++ manage.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+
+import time
+
 print("Running the manage file!")
+
+x=120
+while x>0:
+    print("This is build is taking a long time! ({} seconds left)".format(x))
+    x -= 1
+    time.sleep(1)
+
+print("Finished at last")
+
